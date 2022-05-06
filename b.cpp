fa941094b12223474a7698b3dde828eb7edf53a9
yxfish13 <yxfish13@gmail.com>
2022-05-06 12:54:45 +0800

    版本2

---
+++ b.cpp
@@ -0,0 +1,11 @@
+#include<cstdio>
+#include<cstring>
+#include<iostream>
+
+using namespace std;
+
+int main(){
+    cout<<"hello beijing"<<endl;
+    // cout<<"hello world"<<endl;
+    return 0;
+}
